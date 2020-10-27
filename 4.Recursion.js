525566b6ed429d35e1ee61365e916a450c5bc5a8
DinaTaklit <ez_taklit@esi.dz>
2020-10-27 22:39:58 +0100

    4.Recursion: advanced example to show the utility of recursion

--- 4.Recursion.js
+++ 4.Recursion.js
@@ -5,4 +5,56 @@ const countDownForm = num => {
     countDownForm(num -1);
 }
 
-countDownForm(10);
+// countDownForm(10);
+
+//=============================================
+// Recursion : advanced example.
+
+const categories = [
+    { id: 'animals', parent: null},
+    { id: 'mamals', parent: 'animals'},
+    { id: 'cats', parent: 'mamals'},
+    { id: 'dogs', parent: 'mamals'},
+    { id: 'chihuahua', parent: 'dogs'},
+    { id: 'labrador', parent: 'dogs'},
+    { id: 'persian', parent: 'cats'},
+    { id: 'siamese', parent: 'cats'}
+]
+
+/***
+ * We want to get this tree
+{
+    "animals": {
+        "mamals": {
+            "cats": {
+                "persian": null,
+                "siamese": null,
+            }
+            "dogs": {
+                "chihuahua": null,
+                "labrador": null
+            }
+        }
+    }
+}
+*****/
+
+// So let us use recursion to solve this problem
+
+
+const makeTree = (categories, parent) => {
+    let node = {};
+    categories
+        .filter(category => category.parent === parent)
+        .forEach(category => 
+            node[category.id] = makeTree (categories, category.id))
+    return node;
+}
+
+console.log(
+    JSON.stringify(
+        makeTree(categories, null),
+        null,
+        2
+    )
+);
