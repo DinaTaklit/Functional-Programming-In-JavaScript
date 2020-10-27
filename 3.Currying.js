123adaad9f5d677889e93b390bac32b9d47f18fc
DinaTaklit <ez_taklit@esi.dz>
2020-10-27 21:42:59 +0100

    3.Currying wrap up two files in single file

--- 3.Currying.js
+++ 3.Currying.js
@@ -17,3 +17,24 @@ console.log(dragon_currying('fluffykins')('tiny')('lighthning'));
 // Transfor a normal function into currying function
 const dragon_nrmal_to_currying = _.curry(dragon_nrmal);
 console.log(dragon_nrmal_to_currying('fluffykins')('tiny')('lighthning'));
+
+
+//========================================================================
+
+// Advanced example to show why currying is useful
+let dragons = [
+    {name: 'fluffykins', element: 'lighthning'},
+    {name: 'noomi', element: 'lighthning'},
+    {name: 'karo', element: 'fire'},
+    {name: 'doomer', element: 'timewrap'},
+]
+
+const hasElement = (element, item) => item.element === element;
+let lighthningDragons = dragons.filter( item => hasElement('lighthning', item));
+console.log(lighthningDragons);
+
+// Now see the improvement with currying
+
+const hasElement_curry = _.curry(hasElement);
+let lighthningDragons_curry = dragons.filter(hasElement_curry('lighthning'));
+console.log(lighthningDragons_curry);
