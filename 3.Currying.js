c6004363b980f93c411297cfe4dd6c273ceb0a00
DinaTaklit <ez_taklit@esi.dz>
2020-10-27 21:10:02 +0100

    3.Curring: Transform nrml function into curry function using  _.curry function from lodash library

--- 3.Currying.js
+++ 3.Currying.js
@@ -1,3 +1,4 @@
+import _ from './node_modules/lodash/lodash.js'
 
 // A normal function
 const dragon_nrmal = (name, size, element) => 
@@ -12,3 +13,7 @@ const dragon_currying =
     element => `${name} is a ${size} dragon that breathes ${element}!`
     
 console.log(dragon_currying('fluffykins')('tiny')('lighthning'));
+
+// Transfor a normal function into currying function
+const dragon_nrmal_to_currying = _.curry(dragon_nrmal);
+console.log(dragon_nrmal_to_currying('fluffykins')('tiny')('lighthning'));
